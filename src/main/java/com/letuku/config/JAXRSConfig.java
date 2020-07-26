04da5c2c1d3178dc193e2b12f5c5c7391ef337fb
Jeremiah Kgoroba <jeremiah.kgoroba@standardbank.co.za>
2020-07-26 23:18:33 +0200

    Added a jaxrs config

---
+++ src/main/java/com/letuku/config/JAXRSConfig.java
@@ -0,0 +1,8 @@
+package com.letuku.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api/v1")
+public class JAXRSConfig extends Application {
+}
